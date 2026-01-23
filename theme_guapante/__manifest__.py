92e194436a136d3e5779589c7cce805a1ebf4be7
David Alexander Hernandez Arango <davidhdzara@outlook.com>
2026-01-23 18:29:26 -0500

    Fix Native Sidebar: Inject categories context into product page

--- theme_guapante/__manifest__.py
+++ theme_guapante/__manifest__.py
@@ -17,6 +17,7 @@
         'views/shop/categories.xml',
         'views/shop/products_item.xml',
         'views/shop/layout.xml',
+        'views/shop/product.xml',
         'views/auth/login.xml',
         'views/auth/signup.xml',
         'views/snippets/s_hero.xml',
