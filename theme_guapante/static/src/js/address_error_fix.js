c17fa744ce4fdb0d1647199f7767d28755618085
David Alexander Hernandez Arango <davidhdzara@outlook.com>
2026-02-13 15:11:23 -0500

    fix: crear archivos faltantes en manifest (checkout.xml, .scss, address_error_fix.js) para solucionar error 500

---
+++ theme_guapante/static/src/js/address_error_fix.js
@@ -0,0 +1,6 @@
+/** @odoo-module **/
+
+// This file is currently empty to satisfy manifest import.
+// It was intended to fix address/checkout related JS errors.
+// TODO: Implement address error handling logic if needed.
+console.log('Guapante Address Error Fix loaded');
