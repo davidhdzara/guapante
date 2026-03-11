a70ccada8d52dea9e680b65548bbf501f2dfe926
David Alexander Hernandez Arango <davidhdzara@outlook.com>
2026-03-11 16:30:38 -0500

    [FIX] Add preparation_list.js to backend assets

--- theme_guapante/__manifest__.py
+++ theme_guapante/__manifest__.py
@@ -92,6 +92,9 @@
             'theme_guapante/static/src/js/seasonal_harvest.js',  # Seasonal products dynamic loader
             'theme_guapante/static/src/js/search_modal.js',  # Full-screen search overlay
         ],
+        'web.assets_backend': [
+            'theme_guapante/static/src/js/preparation_list.js',
+        ],
     },
     'application': False,
     'auto_install': False,
