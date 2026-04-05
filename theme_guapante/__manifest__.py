087f0e15a08801bf71f01a60f28e32be26ab77de
jeff <jeffreysaa@outlook.com>
2026-04-04 20:27:31 -0500

    feat: daily preparation time windows fixed

--- theme_guapante/__manifest__.py
+++ theme_guapante/__manifest__.py
@@ -3,7 +3,7 @@
     'description': 'Modern, fresh theme for Guapante based on Odoo 18 best practices.',
     'category': 'Theme/eCommerce',
     'summary': 'Fresh, Organic, Modern',
-    'version': '2.0.8',
+    'version': '2.0.9',
     'images': [
         'static/description/guapante_preview.png',
         'static/description/icon.png',
